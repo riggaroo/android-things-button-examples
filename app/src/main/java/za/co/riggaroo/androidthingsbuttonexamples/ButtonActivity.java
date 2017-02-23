0abe65ef2fc70d3b1757054bcaac942878493696
Rebecca Franks <franks.rebecca@gmail.com>
2017-02-23 19:23:52 +0200

    Using Button Driver from android-things contrib driver.

--- app/src/main/java/za/co/riggaroo/androidthingsbuttonexamples/ButtonActivity.java
+++ app/src/main/java/za/co/riggaroo/androidthingsbuttonexamples/ButtonActivity.java
@@ -3,10 +3,9 @@ package za.co.riggaroo.androidthingsbuttonexamples;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
-import com.google.android.things.pio.Gpio;
-import com.google.android.things.pio.GpioCallback;
-import com.google.android.things.pio.PeripheralManagerService;
+import com.google.android.things.contrib.driver.button.Button;
 
 import java.io.IOException;
 
@@ -14,51 +13,40 @@ import static android.content.ContentValues.TAG;
 
 public class ButtonActivity extends Activity {
 
-    private Gpio buttonGpio;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button);
 
-        PeripheralManagerService service = new PeripheralManagerService();
-
         try {
-            buttonGpio = service.openGpio(BoardDefaults.getGPIOForButton());
-            // Configure the peripheral
-            buttonGpio.setDirection(Gpio.DIRECTION_IN);
-            buttonGpio.setEdgeTriggerType(Gpio.EDGE_FALLING);
-            // Attach callback for input events
-            buttonGpio.registerGpioCallback(new GpioCallback() {
+            button = new Button(BoardDefaults.getGPIOForButton(), Button.LogicState.PRESSED_WHEN_LOW);
+            button.setOnButtonEventListener(new Button.OnButtonEventListener() {
                 @Override
-                public boolean onGpioEdge(Gpio gpio) {
-                    try {
-                        Log.i(TAG, "GPIO value changed:" + gpio.getValue());
-                        // Return true to continue listening to events
-                        return true;
-                    } catch (IOException e) {
-                        Log.e(TAG, "IO Exception reading value out");
-                        return false;
+                public void onButtonEvent(final Button button, final boolean pressed) {
+                    Log.i(TAG, "Button value changed:" + pressed);
+                    if (pressed) {
+                        Toast.makeText(getApplicationContext(), "Button pressed, yay!", Toast.LENGTH_SHORT).show();
                     }
                 }
             });
+
         } catch (IOException ioException) {
-            Log.d(TAG, "Failed to register GPIO callback", ioException);
+            Log.d(TAG, "Failed to register button callback", ioException);
         }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (buttonGpio != null) {
-            // Close the Gpio pin
-            Log.i(TAG, "Closing Button GPIO pin");
+        if (button != null) {
             try {
-                buttonGpio.close();
+                button.close();
             } catch (IOException e) {
                 Log.e(TAG, "Error on PeripheralIO API", e);
             } finally {
-                buttonGpio = null;
+                button = null;
             }
         }
     }
